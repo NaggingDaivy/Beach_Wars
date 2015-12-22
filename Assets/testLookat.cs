31b2e6f3542456d9706e72df29952bd434590e38
NaggingDaivy <daivy.merlijs@hotmail.com>
2015-12-23 00:20:22 +0100

    - little changes

---
+++ Assets/testLookat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testLookat : MonoBehaviour {
+
+    public Transform obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        transform.LookAt(obj.position); ;
+
+        transform.Rotate(-90, 0, 0, Space.Self);
+	
+	}
+}

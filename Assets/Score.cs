fc737adcefe84183bcc2ae693ca6ad3dbc286540
NaggingDaivy <daivy.merlijs@hotmail.com>
2015-12-22 21:55:19 +0100

    - Add script for counting score

---
+++ Assets/Score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    private int _score  = 0;
+	// Use this for initialization
+	void Start () {
+        UpdateScore(_score);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void UpdateScore(int score)
+    {
+        _score += score;
+
+        GetComponent<Text>().text = "Score: " + score;
+    }
+}

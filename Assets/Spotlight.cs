fa0098d55c796b1f29ccf45c8231bd4eba72ed18
NaggingDaivy <daivy.merlijs@hotmail.com>
2016-01-06 17:03:44 +0100

    - Add script for gyrophare

---
+++ Assets/Spotlight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spotlight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0,10,0);
+	
+	}
+}

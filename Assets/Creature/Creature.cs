7dd8d3d4633fc788f1ecb8e0c96d80d295004585
NaggingDaivy <daivy.merlijs@hotmail.com>
2016-01-07 22:32:42 +0100

    - Add creature and animation

---
+++ Assets/Creature/Creature.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creature : MonoBehaviour
+{
+
+    private Animator _Animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _Animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.Space)
+            && !_Animator.GetCurrentAnimatorStateInfo(0).IsName("jump_state"))
+        {
+            _Animator.SetTrigger("jump_trigger");
+        }
+	
+	}
+}

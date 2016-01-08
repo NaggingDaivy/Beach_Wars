2a0d82eef4ff6078049c04165b9a155b249e96a3
NaggingDaivy <daivy.merlijs@hotmail.com>
2016-01-08 22:00:30 +0100

    - Camera logic to follow player

--- Assets/Code/Camera/CameraFollowPlayer.cs
+++ Assets/Code/Camera/CameraFollowPlayer.cs
@@ -4,15 +4,23 @@ using System.Collections;
 public class CameraFollowPlayer : MonoBehaviour
 {
 
-    public Transform Player;
-    public Vector3 DistanceCameraFromPlayer;
+    public BasePlayer _Player;
+    //public Vector3 DistanceCameraFromPlayer;
 
-    public float CameraOffset;
+    public CameraMode _CameraMode = CameraMode.Normal;
+
+    private Transform DefaultCameraPosition;
+
+    //public bool CanRotate = true;
+
+    //public float CameraOffset;
 
     // Use this for initialization
     void Start()
     {
-        Player.GetComponent<IControllable>().EnableInput();
+        //Player.GetComponent<IControllable>().EnableInput();
+
+        
 
     }
 
@@ -20,13 +28,41 @@ public class CameraFollowPlayer : MonoBehaviour
     void Update()
     {
 
-        Vector3 destinationPosition = Player.position + (Player.rotation*DistanceCameraFromPlayer);
+        //Vector3 destinationPosition = Player.gameObject.transform.position + (Player.gameObject.transform.rotation*DistanceCameraFromPlayer);
 
 
-        this.transform.position = Vector3.Lerp(this.transform.position, destinationPosition, CameraOffset*Time.deltaTime);
+        //this.transform.position = Vector3.Lerp(this.transform.position, destinationPosition, CameraOffset*Time.deltaTime);
 
 
-        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Player.rotation, CameraOffset*Time.deltaTime);
+        //this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Player.gameObject.transform.rotation, CameraOffset*Time.deltaTime);
+
+        //transform.RotateAround(_Player.transform.position,Vector3.up,2);
+
+        switch (_CameraMode)
+        {
+                case CameraMode.Normal:
+            {
+                transform.RotateAround(_Player.transform.position, _Player.transform.up, Input.GetAxis("RightH") * 2);
+                transform.RotateAround(_Player.transform.position, transform.right, Input.GetAxis("RightV") * 2);
+                
+                
+                
+               
+                
+                break;
+            }
+            case CameraMode.Fixed:
+            {
+                break;
+            }
+
+                case CameraMode.Free:
+            {
+                break;
+            }
+        }
+
+
 
 
     }

ca9ed4882d08c290fdfc544641c620bba897072a
Daivy Merlijs <daivy.merlijs@hotmail.com>
2016-01-11 16:31:16 +0100

    - Editor for spawning decorations

---
+++ Assets/Editor/DecorationSpawnerEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CanEditMultipleObjects, CustomEditor(typeof(DecorationSpawner))]
+public class DecorationSpawnerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Respawn decorations"))
+        {
+            ((DecorationSpawner)target).SpawnDecoration();
+        }
+
+        if (GUILayout.Button("Remove decorations"))
+        {
+            ((DecorationSpawner)target).RemoveDecoration();
+        }
+    }
+
+	
+}

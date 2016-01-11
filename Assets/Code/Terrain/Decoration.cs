beed413ccbd4aa444f9d3ebe0210ab9e05b1ddff
Daivy Merlijs <daivy.merlijs@hotmail.com>
2016-01-11 16:25:26 +0100

    - Add laser neon to laser mesh

---
+++ Assets/Code/Terrain/Decoration.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Decoration : MonoBehaviour {
+
+    public float minimalAltitude = 0.0f;
+    public float maximalAltitude = 20.0f;
+
+    public bool randomizeXOrientation = false;
+    public bool randomizeYOrientation = true;
+    public bool randomizeZOrientation = false;
+
+    public bool useTerrainOrientation = true;
+
+    public float maximalDepth = 0.5f;
+
+    public float scaleRandomizationIntensity = 0.2f;
+    public bool scaleRandomizatioinUniform = true;
+
+    public bool isAllowed(float altitude)
+    {
+        return ((altitude >= minimalAltitude) && (altitude <= maximalAltitude));
+    }
+
+    public float getRandomAltitude(float altitude)
+    {
+        return altitude - Random.Range(0.0f, maximalDepth);
+    }
+
+    public Vector3 getRandomScale()
+    {
+        if (scaleRandomizatioinUniform)
+        {
+            float scaleRnd = 1.0f + Random.Range(0.0f, scaleRandomizationIntensity);
+            return new Vector3(scaleRnd, scaleRnd, scaleRnd);
+        }
+        else
+        {
+            Vector3 scaleRnd = Random.insideUnitSphere; //Donne un vector 3 alléatoire de max de 1 (Attention aussi valleurs négative)
+            scaleRnd.x = Mathf.Abs(scaleRnd.x);
+            scaleRnd.y = Mathf.Abs(scaleRnd.y);
+            scaleRnd.z = Mathf.Abs(scaleRnd.z);
+            return Vector3.one + scaleRnd * scaleRandomizationIntensity; // Vector3.one = (1,1,1)
+        }
+    }
+
+    public Quaternion getRandomOrientation(Vector3 normal)
+    {
+        Quaternion originalRot;
+
+        if (useTerrainOrientation)
+        {
+            originalRot = Quaternion.LookRotation(normal, Vector3.forward) * Quaternion.Euler(new Vector3(90.0f, 0.0f, 0.0f)); // Forward de l'objet qui regarde dans l'orientatiopn de la normal du terrain
+            // * 90.0f car c'est le Z qui oriente l'objet -> on rederesse l'objet
+        }
+        else
+        {
+            originalRot = Quaternion.identity;
+        }
+
+        Vector3 xyzRot = Vector3.zero;
+        if (randomizeXOrientation)
+            xyzRot.x = 1.0f;
+        if (randomizeYOrientation)
+            xyzRot.y = 1.0f;
+        if (randomizeZOrientation)
+            xyzRot.z = 1.0f;
+
+        Quaternion rndRot = Quaternion.Euler(xyzRot.x * Random.Range(0.0f, 360.0f), xyzRot.y * Random.Range(0.0f, 360.0f), xyzRot.z * Random.Range(0.0f, 360.0f));
+
+        return originalRot * rndRot;
+    }
+	
+}
---
+++ Assets/Code/Terrain/DecorationSpawner.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class DecorationSpawner : MonoBehaviour {
+
+    [System.Serializable]
+    public class decoInfo
+    {
+        public List<Decoration> decorationReferences;
+        public int decorationCount = 10;
+        public float minimalDistanceBetweenDecoration = 10.0f;
+    }
+
+    public float minX = 0.0f;
+    public float maxX = 180.0f;
+    public float minZ = 0.0f;
+    public float maxZ = 180.0f;
+
+    public List<decoInfo> decorations = new List<decoInfo>();
+
+    //private float Scale = 0.5f;
+    private GameObject Deco;
+    private TerrainGenerator Terrain;
+
+    public void RemoveDecoration()
+    {
+        foreach (Decoration item in Object.FindObjectsOfType<Decoration>())
+        {
+            DestroyImmediate(item.gameObject);
+        }
+        DestroyImmediate(Deco.gameObject);
+    }
+
+    public void SpawnDecoration()
+    {
+        Terrain = GetComponent<TerrainGenerator>();
+
+        if (Deco == null)
+        {
+            Deco = new GameObject("Decorations");
+        }
+
+        foreach (decoInfo item in decorations)
+        {
+            for (int i = 0; i < item.decorationCount; i++)
+            {
+                int iObject = Random.Range(0, item.decorationReferences.Count);
+
+                bool bPlaced = false;
+                int counter = 0;
+                while ((bPlaced == false) && (counter < 10))
+                {
+                    item.decorationReferences[iObject].transform.position = new Vector3(Random.Range(minX, maxX), 250, Random.Range(minZ, maxZ));
+
+                    RaycastHit hit;
+                    Physics.Raycast(item.decorationReferences[iObject].transform.position, Vector3.down, out hit);
+
+                    if (hit.transform.gameObject == this.transform.gameObject)
+                    {
+                        if (item.decorationReferences[iObject].isAllowed(hit.point.y) && hit.point.y != Terrain.PlatformHeight)
+                        {
+                            item.decorationReferences[iObject].transform.position = new Vector3(item.decorationReferences[iObject].transform.position.x,
+                                                                                                item.decorationReferences[iObject].getRandomAltitude(hit.point.y),
+                                                                                                item.decorationReferences[iObject].transform.position.z);
+
+                            GameObject tObject = Instantiate(item.decorationReferences[iObject].gameObject, item.decorationReferences[iObject].transform.position, item.decorationReferences[iObject].getRandomOrientation(hit.normal)) as GameObject;
+                            tObject.transform.localScale = item.decorationReferences[iObject].getRandomScale();
+                            tObject.transform.parent = Deco.transform;
+                            bPlaced = true;
+                        }
+                        else counter++;
+                    }
+                    else counter++;
+                }
+            }
+        }
+    }
+}

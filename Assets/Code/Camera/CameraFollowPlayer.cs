74c5623b37a5c873719658dc7a0c491a5323ec3f
NaggingDaivy <daivy.merlijs@hotmail.com>
2016-01-12 23:41:55 +0100

    - Put logic in LateUpdate instead of update

--- Assets/Code/Camera/CameraFollowPlayer.cs
+++ Assets/Code/Camera/CameraFollowPlayer.cs
@@ -37,7 +37,7 @@ public class CameraFollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
 
         //Vector3 destinationPosition = Player.gameObject.transform.position + (Player.gameObject.transform.rotation*DistanceCameraFromPlayer);
